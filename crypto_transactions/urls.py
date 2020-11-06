2df2f6d91bfa36f516b7c63207330a81fa0e7db6
Mike2015 <olumichael2015@outlook.com>
2020-11-06 23:03:45 +0100

    axemo-live

--- crypto_transactions/urls.py
+++ crypto_transactions/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('crypto/<currency>', views.crypto, name='crypto'),
     path('receive/', views.receive, name='receive'),
-    path('qr/', views.qrcam, name='qr'),
     path('send/confirm/', views.check, name='check'),
 ]
