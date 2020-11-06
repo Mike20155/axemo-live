8b522db73afbe17dd8b7c97c5bf06c2459ed26d0
Mike2015 <olumichael2015@outlook.com>
2020-11-06 16:48:38 +0100

    live version

--- axemolive/settings.py
+++ axemolive/settings.py
@@ -131,6 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = "/static/"
+STATIC_URL = "home/static/"
 
 django_heroku.settings(locals())
