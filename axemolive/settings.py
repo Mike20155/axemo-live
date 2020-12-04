de6510f78fe58eb2006fd861dcbb1fbfcc48f4e5
Mike2015 <olumichael2015@outlook.com>
2020-12-04 22:40:15 +0100

    update

--- axemolive/settings.py
+++ axemolive/settings.py
@@ -149,7 +149,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '25'
 EMAIL_HOST_USER = 'olumichael2016x@gmail.com'
-EMAIL_HOST_PASSWORD = 'Xenonsugar1##'
+EMAIL_HOST_PASSWORD = 'Xenonsugar1*'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
